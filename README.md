a063bd792803a75ed9ba36930140a7503e08d861
Phil Sinatra <code@philsinatra.com>
2017-02-24 11:53:27 -0500

    📚  Add TODO List

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # IDM 232: Scripting for Interactive Digital Media II
+
+## TODO
+
+- week 6 presentation file
+- week 7 presentation file
+- week 8 presentation file
+- week 8 example files
+- homework assignments
--- examples/week6/admin/includes/_head.php
+++ examples/week6/admin/includes/_head.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Bootstrap for admin panel instead of custom UI. -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <title>IDM Course Catalog - CMS</title>
+  <!-- CKEditor for WYSIWYG within admin panel. -->
   <script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
 </head>
